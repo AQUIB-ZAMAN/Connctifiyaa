550c10f924a7ee6e6817f6bf3fe3333173a56bb2
AQUIB-ZAMAN <64305732+AQUIB-ZAMAN@users.noreply.github.com>
2022-05-27 03:21:11 +0530

    Initial changes to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
