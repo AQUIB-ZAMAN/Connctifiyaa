fba020770a237cfd888ea78d1c3eca5d6768bafa
AQUIB-ZAMAN <64305732+AQUIB-ZAMAN@users.noreply.github.com>
2022-05-30 14:30:21 +0530

    added join meeting screen

--- lib/views/screens/video_call_screen.dart
+++ lib/views/screens/video_call_screen.dart
@@ -1,1 +1,51 @@
+import 'package:flutter/material.dart';
 
+import '../../utilities/colors.dart';
+
+class VideoCallScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: backgroundColor,
+          title: Text(
+            'Join a Meeting',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: Column(children: [
+          TextField(
+            keyboardType: TextInputType.number,
+            maxLines: 1,
+            decoration: InputDecoration(
+              fillColor: secondaryBackgroundColor,
+              filled: true,
+              border: InputBorder.none,
+              hintText: 'Room ID',
+              contentPadding: EdgeInsets.all(10),
+            ),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            maxLines: 1,
+            decoration: InputDecoration(
+              fillColor: secondaryBackgroundColor,
+              filled: true,
+              border: InputBorder.none,
+              hintText: 'User Name',
+              contentPadding: EdgeInsets.all(10),
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ]));
+  }
+}
